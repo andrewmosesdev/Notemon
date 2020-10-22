f3e9e6042c92e47fc403ba6b4ac20d6a5f27a78b
Andrew Moses <andrewmosesdrive@gmail.com>
2020-10-21 22:33:55 -0700

    added basic framework, set up port and listener; set up 2 routes

---
+++ Develop/public/server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+
+// PORT
+// ====================================================
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT http://localhost:" + PORT)
+});
+
+
+
+// ROUTES
+// ====================================================
+app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.json(path.join(__dirname, "public/notes.html"));
+});
